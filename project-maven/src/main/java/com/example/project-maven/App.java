a0f2df91ad6984bed8a0939ba5a258a038859a62
Ubuntu <venky@venkata.ymda4bnu004etkjjurxmji24lb.rx.internal.cloudapp.net>
2024-07-19 05:47:47 +0000

    maven

---
+++ project-maven/src/main/java/com/example/project-maven/App.java
@@ -0,0 +1,13 @@
+package com.example.project-maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
